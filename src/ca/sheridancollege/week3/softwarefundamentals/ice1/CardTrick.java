ec67da969a9a1040ecfb06144539754ee78ea69d
Zhao Wang <49773470+Egnellahc@users.noreply.github.com>
2019-05-25 23:26:33 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,7 +9,8 @@ import java.util.*;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author Zhao Wang
+ * Student Number: 991556434
  */
 public class CardTrick {
     
